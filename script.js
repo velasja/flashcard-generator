f11da01334df729df8b676b13b7b0fbfd0dce976
JJ Velasquez <velas.ja@gmail.com>
2017-01-25 03:08:51 -0600

    finally functional

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// var questions = require("./basic.json");
+var clozeQs = require("./cloze.json");
+var basicQs = require("./basic.json");
+var inquirer = require("inquirer");
+var fs = require("fs");
+var count = 0;
 // Create a BasicCard constructor. It should accept front and back arguments.
 function BasicCard (front, back) {
 	this.front = front,
@@ -24,5 +30,78 @@ function ClozeCard (text, cloze) {
 	}
 };
 
-var card = new ClozeCard("the president is", "donald trump");
-	console.log(card);
+function restart () {
+	inquirer.prompt([
+		{
+			type: "confirm",
+			name: "restart",
+			message: "Go back to the start?"
+		}
+	]).then(function(answers) {
+		if (answers.restart === true) {
+			basicFlashCard();
+		} else {
+			console.log("Goodbye!");
+			process.exit();
+		}
+	});
+};
+// var card = new ClozeCard(clozeQs.front[count], clozeQs.back[count]);
+
+var basicFlashCard = function() {
+	if (count < basicQs.length) {
+		inquirer.prompt([
+			{
+				name: "basic",
+				type: "input",
+				message: basicQs[count].front
+			}
+		]).then(function(answers) {
+			
+			if (answers.basic === basicQs[count].back) {
+				console.log("Correct!");
+			} else {
+				console.log("I'm sorry, that's the wrong answer.");
+			};
+			count++;
+			basicFlashCard();
+		});
+	} else {
+		console.log("You completed the flashcards!");
+		inquirer.prompt([
+			{
+				type: "confirm",
+				name: "ask",
+				message: "Do you want to create a new flashcard?"
+			}
+		]).then(function(answers) {
+			if (answers.ask === true) {
+				inquirer.prompt([
+					{
+						type: "input",
+						name: "front",
+						message: "Type the front of your flashcard:"
+					}, {
+						type: "input",
+						name: "back",
+						message: "Type the back of your flashcard:"
+					}
+				]).then(function(answers) {
+					var card = new BasicCard(answers.front, answers.back);
+					basicQs.push(card);
+					fs.writeFile('basic.json', JSON.stringify(basicQs), (err) => {
+					  if (err) throw err;
+					});
+					count = 0;
+					restart();
+				});
+			} else {
+				count = 0;
+				restart();
+			}
+		
+		});
+	};
+};
+
+basicFlashCard();
